ea3aafd10414324bce2f14aa685f153cc5c56808
Evgeny Artelny <eartelny@gmail.com>
2020-11-18 00:16:23 +0300

    working parkour

---
+++ Assets/Script/hurB.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class hurB : MonoBehaviour
+{
+    void Start()
+    { }
+    private bool OnWater;
+     
+    private void OnTriggerEnter(Collider collision)
+    {
+        OnWater = false;
+        if (collision.gameObject.tag == "Water")
+        {
+            OnWater = true;
+            
+       }
+        if(OnWater == true)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
